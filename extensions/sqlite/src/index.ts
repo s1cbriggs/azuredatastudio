30d5ac98ac6c0a69c942b582137f10f5847419a9
Anthony Dresser <andresse@microsoft.com>
2019-12-02 18:39:11 -0800

    init sqlitre

---
+++ extensions/sqlite/src/index.ts
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import * as sqlite from 'sqlite3';
+
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+}
+
+export async function deactivate(): Promise<any> {
+}
